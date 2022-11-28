304a36941195028335c08b4ad1b0aff083433fb0
kyw0716 <kyw0716@naver.com>
2022-11-29 00:33:39 +0900

    fix: numberSize 상수 출력 문구에 반영되지 않는 부분 수정

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -2,7 +2,7 @@ const GuideString = Object.freeze({
   START: "숫자 야구 게임을 시작합니다.",
   INPUT_NUMBER: "숫자를 입력해주세요 : ",
   RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-  END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  END: `${numberSize}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
 });
 
 const ErrorString = Object.freeze({
